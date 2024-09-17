c06e3958d8bf739fbdc08deff0805f948e286321
J. Israel <147664029+JSRomero007@users.noreply.github.com>
2024-09-17 12:46:18 -0600

    Add files via upload

---
+++ Global/DBConect.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+
+namespace ScaningSystem.Global
+{
+    internal class DBConect
+    {
+        public SQLiteConnection myConnection;
+        public DBConect()
+        {
+
+            
+            string conectionString= @"Data Source="+Global.Global_V.URLstringSQLite + "; Version=3";
+            myConnection = new SQLiteConnection(conectionString);
+            if (!File.Exists(Global.Global_V.URLstringSQLite))
+            {
+               
+                Console.WriteLine("Database Not found ");
+            }
+    
+            
+        }
+        public void OpenConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+        }
+        public void CloseConnection() 
+        {
+            if (myConnection.State != System.Data.ConnectionState.Closed)
+            {
+                myConnection.Close();
+            }
+        }
+    }
+}
---
+++ Global/Global_V.cs
@@ -0,0 +1,59 @@
+﻿/**
+*   this code concatenates the information obtained from the Keyence IV2-50P system and moves the other folder.
+*   @Param
+*        --identify txt and jpg files from a specific folder
+*   @Return
+*        --rename and move files to folder
+*   @Version 28.04.23
+*   @sinse 07-04-2023
+*   @author <jose.israel.cosme@aptiv.com>
+*/
+//------------ Using Library ------------
+using System;
+using System.Collections.Generic;
+//---------------------------------------
+
+namespace ScaningSystem.Global
+{
+    public static class extencion //Subroutine to remove extension
+    {
+        public static string Filter(this string str, List<string> ChartRemove)
+        {
+            foreach (string c in ChartRemove)
+            {
+                str = str.Replace(c.ToString(), String.Empty);
+            }
+            return str;
+        }
+    }
+    internal class Global_V //Usinf global variables 
+    {
+        public static string Turn;
+        public static string Operator;
+        public static string Shift_A,Shift_B,Shift_C;
+        public static int CountToReset;
+        public static string URL = "";
+        public static string BarCode_1 = "1", BarCode_2 = "2";
+        public static string Jpeg, Z_code, str, Save_Z_Code;
+        public static string Step_1, Step_2, Step_3;
+        public static string UB,Console,Remove;
+        public static string V = "V.6.17.2023";
+        public static string ZCode, Date, Time, Meridiam;
+        public static string ZcodeFilter;
+        //--------Constructor--------//
+        public static string PhatINIconstructor= @"C:\Users\Public\Pictures\DinoCaptureSystem\Config\DinoConfig.ini";
+        public static string PhatShiftconstructor = @"C:\Users\Public\Pictures\DinoCaptureSystem\Config\";
+        public static string Constructor;
+        public static string URLstringSQLite;
+        public static string URLDinoSaveCodeZ;
+        public static string Search; //path to search .jpeg and .txt file
+        public static string VSearch; //path to search .wmv file
+        public static string Bakup; //path to backup
+        public static string SqlZcode;
+        public static string SqlDate;
+        public static string SqlTime;
+        public static string SqlOperator;
+        public static string SqlShift;
+
+    }
+}
